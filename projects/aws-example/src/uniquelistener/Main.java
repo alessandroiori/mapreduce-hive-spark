63b02443489f644d83d1b78e7c78b08e4dfc6073
alessandroiori <alessandro.iori92@gmail.com>
2017-04-15 17:40:15 +0200

    added .csv file, up uniquelistener project

--- projects/aws-example/src/uniquelistener/Main.java
+++ projects/aws-example/src/uniquelistener/Main.java
@@ -3,24 +3,26 @@ package uniquelistener;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.mapred.Counters;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-import uniquelistener.Map.COUNTERS;
-
 
 public class Main extends Configured implements Tool{
+
+	public enum COUNTERS {
+		INVALID_RECORD_COUNT,
+		VALID_RECORD_COUNT
+	}
 	
-	private static org.apache.hadoop.mapreduce.Counters counters;
+	//private static org.apache.hadoop.mapreduce.Counters counters;
 	
 	@Override
 	public int run(String[] args) throws Exception {
 		if(args.length != 2) {
-			System.out.println("Usage: uniquelisteners < in > < out >");
+			System.out.println("Usage: uniquelisteners/Main < in > < out >");
 			System.exit(2);
 		}
 		
@@ -39,7 +41,7 @@ public class Main extends Configured implements Tool{
 		
 		//return job.waitForCompletion(true) ? 0 : 1;
 		int exitCode = job.waitForCompletion(true) ? 0 : 1;
-		counters = job.getCounters();
+		//counters = job.getCounters();
 		
 		return exitCode;
 	}
@@ -47,8 +49,11 @@ public class Main extends Configured implements Tool{
 	public static void main(String args[]) throws Exception{
 		int exitCode = ToolRunner.run(new Main(), args);
 		System.exit(exitCode);
-		System.out.println("No. of Invalid Records :"
-				 + counters.findCounter(COUNTERS.INVALID_RECORD_COUNT)
-				 .getValue());
+//		System.out.println("No. of Invalid Records :"
+//				 + counters.findCounter(COUNTERS.INVALID_RECORD_COUNT)
+//				 .getValue());
+//		System.out.println("No. of Valid Records :"
+//				 + counters.findCounter(COUNTERS.VALID_RECORD_COUNT)
+//				 .getValue());
 	}
 }
--- projects/aws-example/src/uniquelistener/Map.java
+++ projects/aws-example/src/uniquelistener/Map.java
@@ -3,8 +3,12 @@ package uniquelistener;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+
+//import org.apache.hadoop.mapreduce.Counters;
+//import uniquelistener.Main.COUNTERS;
 
 /**
  * 
@@ -12,39 +16,34 @@ import org.apache.hadoop.mapreduce.Mapper;
  * @date	15-04-2017
  *
  */
-public class Map extends Mapper<IntWritable, Text, IntWritable, IntWritable> {
-
-	IntWritable userId = new IntWritable();
-	IntWritable trackId = new IntWritable();
-
-	public static enum COUNTERS {
-		INVALID_RECORD_COUNT,
-		VALID_RECORD_COUNT
-	};
-
-	@Override
-	public void map(IntWritable key, Text value, 
-			Mapper<IntWritable, Text, IntWritable, IntWritable>.Context context) 
-					throws IOException, InterruptedException {
-
-		String[] parts=value.toString().split("|");
-
-		try {
-			userId.set(Integer.parseInt(parts[0]));
-			trackId.set(Integer.parseInt(parts[1]));
+public class Map extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+	
+		IntWritable userId = new IntWritable();
+		IntWritable trackId = new IntWritable();
+	
+		@Override
+		public void map(LongWritable key, Text value, 
+				Mapper<LongWritable, Text, IntWritable, IntWritable>.Context context) 
+						throws IOException, InterruptedException {
+	
+			String[] parts=value.toString().split(",");
+	
+			try {
+				userId.set(Integer.parseInt(parts[0]));
+				trackId.set(Integer.parseInt(parts[1]));
+				
+				if(parts.length == 5){
+					//add counters for valid record
+					//context.getCounter(COUNTERS.VALID_RECORD_COUNT).increment(1L);
 			
-			if(parts.length == 5){
-				//add counters for valid record
-				context.getCounter(COUNTERS.VALID_RECORD_COUNT).increment(1L);
-		
-				context.write(trackId, userId);
-			} else {
-				//add counters for invalid record
-				context.getCounter(COUNTERS.INVALID_RECORD_COUNT).increment(1L);
-			}
-
-		} catch (Exception e){
-			System.out.println(e.getMessage());
-		}		
-	}
+					context.write(trackId, userId);
+				} else {
+					//add counters for invalid record
+					//context.getCounter(COUNTERS.INVALID_RECORD_COUNT).increment(1L);
+				}
+	
+			} catch (Exception e){
+				System.out.println(e.getMessage());
+			}		
+		}
 }

592a41c84960abd9de9b37f5722367aaab0930ed
alessandroiori <alessandro.iori92@gmail.com>
2017-04-24 22:34:29 +0200

    ended ny-crime-spark.py project

--- spark/projects/ny-crime-spark.py
+++ spark/projects/ny-crime-spark.py
@@ -1,5 +1,6 @@
 import wget
 import csv
+import re
 
 from StrinIO import StringIO 
 from collections import namedtuple
@@ -12,7 +13,7 @@ from pyspark import SparkContext
 file="Desktop/dataset/ny-crime-rows.csv"
 
 sc = SparkContext("local", "NY Crime App")
-data = sc.textFile(file).cache()
+data = sc.textFile(file).map(lambda x:x.encode("utf-8")) #.cache()
 
 # data.take(10)
 
@@ -21,7 +22,6 @@ header = data.first()
 
 # Data without header row
 dataWoHeader = data.filter(lambda x: x<>header)
-dataWoHeader = dataWoHeader.map(lambda x:x.encode("utf-8"))
 
 # replace empty fields
 #dataWoHeader = dataWoHeader.map(lambda x: re.sub(r"(?<=,)(?=,)", "-", x))
@@ -30,9 +30,11 @@ dataWoHeader = dataWoHeader.map(lambda x:x.encode("utf-8"))
 
 fields = header.replace("(","_").replace(")","_").replace(" ","_").replace("/","_").split(",")
 
+fieldsNumber=len(header.split(","))
+
 # remove row with incorrect len
-dataWoHeaderCorrect = dataWoHeader.filter(lambda x:len(x.split(","))==len(fields)+1)
-dataWoHeaderIncorrect = dataWoHeader.filter(lambda x:len(x.split(","))!=len(fields)+1)
+dataWoHeaderCorrect = dataWoHeader.filter(lambda x:len(x.split(","))==fieldsNumber+1)
+dataWoHeaderIncorrect = dataWoHeader.filter(lambda x:len(x.split(","))!=fieldsNumber+1)
 
 # Crime class
 Crime = namedtuple("Crime", fields, verbose=True)
@@ -51,10 +53,10 @@ crimes = dataWoHeaderCorrect.map(parse)
 #crimes.map(lambda x:x.Created_Date).countByValue()
 
 #more realistic data 
-crimeFiltered = crime.filter(lambda x: not (x.Offense=="NA" or x.Occurence_Years==''))\
-			.filter(lambda x: int(x.Occurrence_Years)>=2016)
+#crimeFiltered = crime.filter(lambda x: not (x.Offense=="NA" or x.Occurence_Years==''))\
+#			.filter(lambda x: int(x.Occurrence_Years)>=2016)
 
-#crime.map(lambda x:x.Occurrunce_Years).countByValue()
+crimeFilteredLocation = crimes.filter(lambda x: not (x.Location=''))
 
 # exctract coordinates from row
 def extractCoords(location):
@@ -74,14 +76,39 @@ def extractCoords(location):
 # New York
 # - bounding box: -74,2589, 40,4774, -73,7004, 40,9176
 # - source: https://www.flickr.com/places/info/2459115
-min_lat = -74,2589
-min_lng = 40,4774
-max_lat = -73,7004
-max_lng = 40,9176
+min_lng = -74.2589
+min_lat = 40.4774
+max_lng = -73.7004
+max_lat = 40.9176
 
-crimeFinal = crimeFiltered.filter(lambda x:extractLocation(x.Location_1)[0] >= min_lng and \
-					 x.extractLocation(x.Location_1)[0] <= max_lng and \
-					 x.extractLocation(x.Location_1)[1] >= min_lat and \
-					 x.extractLocation(x.Location_1)[1] <= max_lat)
+crimeFilteredNyLocation= crimeFilteredLocation.\
+				filter(lambda x:extractCoords(x.Location)[0] >= min_lat and \
+						extractCoords(x.Location)[0] <= max_lat and \
+						extractCoords(x.Location)[1] >= min_lng and \
+						extractCoords(x.Location)[1] <= max_lng)
+
+#regex = re.compile(r'^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9] (AM|PM)$')
+regex = re.compile(r'^[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]')
+crimeFilteredDate = crimeFilteredNyLocation.filter(lambda x: regex.search(x.Created_Date))
+
+crimeFinal = CrimeFilteredDate
+#crimeFilteredDate.map(lambda x : re.search('[0-9][0-9][0-9][0-9]', x.Created_Date).group()).countByValue()
 
 
+# sudo pip install gmplot
+import gmplot
+
+map_lat = 37.428
+map_lng = -122.145
+
+gmap = gmplot.GoogpleMapPlotter(map_lat,map_lng,16).from_geocode("New York City")
+
+b_lats = crimeFinal.filter(lambda x : re.search('[0-9][0-9][0-9][0-9]', x.Created_Date).group()=="2017")\
+					.map(lambda x: extractCoords(x.Location)[0]).collect()
+
+b_lngs = crimeFinal.filter(lambda x : re.search('[0-9][0-9][0-9][0-9]', x.Created_Date).group()=="2017")\
+                                        .map(lambda x: extractCoords(x.Location)[1]).collect()
+
+#draw map
+gmap.scatter(b_lats, b_lngs, '#DE1515', size=40, marker=False)
+gmap.draw("ny-crime-map.html")
